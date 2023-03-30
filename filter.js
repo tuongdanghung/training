f7c046be646a5b450d2e1d9dfa34de553a1ee58d
tuongdanghung <tuongdanghung37@gmail.com>
2023-03-30 20:48:02 +0700

    hop nhat note va ghi nho API KEY chat GPT

---
+++ filter.js
@@ -0,0 +1,58 @@
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+let filter = arr.filter((item, index) => {
+    console.log("checkfilter: ", item, index);
+    return item > 3
+})
+console.log('filter', filter);
+
+// 
+
+const arr2 = [
+    { name: 'hung1', age: 1 },
+    { name: 'hung2', age: 12 },
+    { name: 'hung3', age: 13 },
+    { name: 'hung4', age: 14 },
+    { name: 'hung5', age: 15 },
+    { name: 'hung6', age: 16 }
+]
+
+let filter2 = arr2.filter((item) => item.age > 14)
+console.log("filter2", filter2);
+
+let find = arr2.find((item) => item.age > 14)
+console.log("find", find);
+// 
+
+const array1 = [5, 12, 8, 130, 44];
+const index = array1.findIndex((item) => item > 44)
+console.log("index", index);
+// 
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(0);
+console.log("citrus", citrus);
+// 
+
+const splice = citrus.splice(2, 4)
+console.log("citrus", splice);
+
+// 
+const str = "apple,banana,orange";
+const arr3 = str.split(",");
+console.log('split', arr3);
+// 
+
+const arr4 = ['c', 'a', 'd', 'b'];
+arr4.sort();
+console.log("sort a-z", arr4);
+
+const arr5 = ['c', 'a', 'd', 'b'];
+arr5.sort((a, b) => b.localeCompare(a))
+console.log("sort z-a", arr5);
+
+const data = [5, 1, 6, 9, 3];
+const sort1 = data.sort((a, b) => a - b)
+console.log("sort 1-9", sort1)
+const sort2 = data.sort((a, b) => b - a)
+console.log("sort 9-1", sort2)
---
+++ for.js
@@ -0,0 +1,6 @@
+const array = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log("i", i);
+}
